942f21e58e51301023fcfa5b612535fac8192aea
ros1463 <ros1463@yandex.ru>
2017-11-19 22:06:41 +0200

    MVVM added

---
+++ TaskList/TaskList/Command/DelegateCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TaskList.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null ? true: this._canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+        public DelegateCommand(Action<object> exec, Predicate<object> canExec)
+        {
+            this._canExecute = canExec;
+            this._execute = exec;
+        }
+    }
+}
